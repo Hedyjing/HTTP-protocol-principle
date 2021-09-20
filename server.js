1364eab5820653f2a3c1229b3a2080b207e30fc5
Hedyjing <374884275@qq.com>
2021-09-20 18:51:08 +0800

    Cache-Control初使用

--- server.js
+++ server.js
@@ -3,13 +3,25 @@ const fs = require('fs')
 
 http.createServer((request, response) => {
   console.log('request come', request.url)
-
-  const html = fs.readFileSync('test.html', 'utf8');
-  response.writeHead(200, {
-    'Content-Type': 'text/html'
-  })
-  response.end(html);
-  
+  if(request.url === '/') {
+    const html = fs.readFileSync('test.html', 'utf8');
+    response.writeHead(200, {
+      'Content-Type': 'text/html',
+    })
+    response.end(html);
+    
+  }
+  // 如果js文件的内容改变了, 那么在缓存时间内, 客户端的js依然没有变
+  // 要解决这个问题可以通过把这种静态文件生成hash值附在文件名后面,
+  // 这样当文件内容改变时, hash变了, url就变了, 客户端就会再次请求服务器不使用缓存
+  if (request.url === '/script.js') {
+    response.writeHead(200, {
+      'Content-Type': 'text/javascript',
+      'Cache-Control': 'max-age=1000'
+    })
+    response.end("console.log('这个是js文件中打印的')");
+    
+  }
 }).listen(8888)
 
 console.log('server listening on 8888');
